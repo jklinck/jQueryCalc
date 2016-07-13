e4b539663223d7652ab49514d92c463949beb8ce
jklinck <joeklinck@hotmail.com>
2016-07-13 00:45:27 -0700

    got plusMinus function working correctly

--- index.js
+++ index.js
@@ -1,33 +1,63 @@
 $(function(){
-	var value = "";
-	var current = "";
-	var answer = "";
+	var value = ""; // displays user input in text area
+	var value2 = ""; // collects inputs for answerArr
+	var current = ""; // collects current user input
+	var answer = ""; // uses parser.js to evaluate "value" variable
+	var answerArr = []; // separates inputs into distinct numbers and operators
+	var currentIndex = 0; // used to increment indices of answerArr
 
 	// place input into textarea
 	$(".input").on("click",function(){
 		current = $(this).attr("value");
-		value += current;
-		$(".text").html(value);
+		if(current == "+" || current == "-" || current == "*" || current == "/"){
+			currentIndex++;
+			answerArr[currentIndex] = current;
+			value += current;
+			currentIndex++;
+			value2 = "";
+			$(".text").html(value);
+		}
+		else{
+			value += current;
+			value2 += current; // kept track of to populate answerArr[]
+			answerArr[currentIndex] = value2;
+			$(".text").html(value);
+		}
 	});
 
 	// // change value from positive to negative or negative to positive
 	$(".plusMinus").on("click",function(){
-		// in order to get this working I need to separate each number and operator into separate indices inside of an array, at which point I can perform a +/- operation on that individual number and then in the equals above I can loop through it and concatenate it into the answer variable to be processed by Parser.evaluate()
+		answerArr[currentIndex] = "(" + (answerArr[currentIndex]) * (-1) + ")";
+		// the new value of answerArr[currentIndex] needs to be put in parethesis otherwise parser.js will throw a parity error
+		value = ""; 
+		// value is cleared so it can be repopulated wit the for loop below
+		for(var i=0;i<answerArr.length;i++){
+			value += answerArr[i];
+		}
+		$(".text").html(value);
 	});
 
-	// // equals
+	// equals
 	$(".equals").on("click",function(){
 		// user Parser.evaulate function from parser.js
 		answer = Parser.evaluate(value);
 		$(".text").html(answer);
 		value = answer;
+		current = "";
+		value2 = "";
+		answer = "";
+		currentIndex = 0;
+		answerArr = [value]; // this is done so the only value left in answerArr[] is the answer from the previous calculation and can thus start a new calculation
 	});
 
 	// clear
 	$(".clear").on("click",function(){
 		value = "";
+		value2 = "";
 		current = "";
 		answer = "";
+		currentIndex = 0;
+		answerArr = [];
 		$(".text").html(value);
 	});
 });
